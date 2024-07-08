4b26b65ae95d9e280337c472ecb6c8bfcb805425
Kamlesh Paul <kamleshpaul414@gmail.com>
2024-07-08 15:37:16 +0530

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
       const scaleFactor = average / 100;
       const limitedScaleFactor = Math.min(5, 1 + scaleFactor);
 
-      if (limitedScaleFactor > 1.5) {
+      if (limitedScaleFactor > 2) {
         console.log('trigger');
 
         if (!firstWhistleDetectedRef.current) {
